5f461c69bc88f8d5e7cadf084371c3d849292934
Daniel Mino <danielmino@wolfsellers.com>
2025-08-12 23:08:18 -0600

    Resolver errores de lint en quote js

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,8 +1,6 @@
 export default function decorate(block) {
-    
-    const [quoteWrapper] = block.children;
-
-    const blockquote = document.createElement("blockquote");
-    blockquote.textContent = quoteWrapper.textContent.trim();
-    quoteWrapper.replaceChildren(blockquote);
+  const [quoteWrapper] = block.children;
+  const blockquote = document.createElement('blockquote');
+  blockquote.textContent = quoteWrapper.textContent.trim();
+  quoteWrapper.replaceChildren(blockquote);
 }
