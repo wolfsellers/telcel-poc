c48b8a34871b0cafe66c053f1e13bf8a6efb41a8
Ernesto Leon <ernestoleon@wolfsellers.com>
2025-08-14 17:42:30 -0600

    feat(banner-promociones): se agrega funcionalidad de slide a banners.

--- blocks/banner_promociones/banner_promociones.js
+++ blocks/banner_promociones/banner_promociones.js
@@ -1,10 +1,10 @@
 import { createSlider } from '../../scripts/slides.js';
 
 export default function decorate(block) {
-    const [wrapper] = block.children;
+  const [wrapper] = block.children;
 
-    createSlider(wrapper, { timeout: 5000 });
+  createSlider(wrapper, { timeout: 5000 });
 
-    console.log(block);
-    console.log(wrapper);
+  console.log(block);
+  console.log(wrapper);
 }
