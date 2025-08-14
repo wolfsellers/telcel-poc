46c731ad5debf3e1d20ced0f0da3d0697ea79143
Ernesto Leon <ernestoleon@wolfsellers.com>
2025-08-14 17:57:33 -0600

    feat(banner-promociones): se agrega funcionalidad de slide a banners.

--- scripts/slides.js
+++ scripts/slides.js
@@ -7,7 +7,7 @@
 export default function createSlider(container, { timeout = 4000 } = {}) {
   const imgslides = container.querySelectorAll('.slide');
   let current = -1;
-  let slider;
+
   function fadeNextSlide() {
     imgslides.forEach((img) => {
       img.style.opacity = 0;
@@ -17,7 +17,7 @@ export default function createSlider(container, { timeout = 4000 } = {}) {
 
     imgslides[current].style.opacity = 1;
 
-    slider = setTimeout(fadeNextSlide, timeout);
+    setTimeout(fadeNextSlide, timeout);
   }
   // Inicia el slider
   fadeNextSlide();
